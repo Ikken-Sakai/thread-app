f8d21bbb832ec5d3ac5c8178a526875948eb6a94
Ikken-Sakai <ikepoppo@yahoo.com>
2025-11-04 10:18:02 +0900

    style.css名前変更→style_login.css

--- login.php
+++ login.php
@@ -63,7 +63,7 @@ header('Content-Type: text/html; charset=UTF-8');
   <meta charset="UTF-8">
   <title>ログイン | スレッドアプリ</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="style_login.css">
 </head>
 <body>
   <div class="login-container">
